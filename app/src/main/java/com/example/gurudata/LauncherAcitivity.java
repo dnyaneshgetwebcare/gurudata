2d50e6754be20bb5fbc4d7746611fcbba141bc2d
dnyanesh <dnyaneshwareh@gmail.com>
2023-03-26 10:29:37 +0530

    remaning

---
+++ app/src/main/java/com/example/gurudata/LauncherAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.gurudata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LauncherAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher_acitivity);
+    }
+}
